1cf8f6032ae783de1229715b16e2330506c815a2
Anna Trybel <annakalinovich717@gmail.com>
2025-11-24 15:16:50 +0100

    refactor: enhance DashboardPage layout and structure

--- budget-frontend/src/app/dashboard/page.tsx
+++ budget-frontend/src/app/dashboard/page.tsx
@@ -1,8 +1,18 @@
+import MetabaseReport from "./MetabaseReport"; 
+
 export default function DashboardPage() {
+    const MY_DASHBOARD_ID = 2; 
+
     return (
-        <div>
-            <h1>Witaj na Dashboardzie!</h1>
-            <p>Jesteś zalogowany.</p>
-        </div>
+        <main className="min-h-screen bg-[#272C3C] text-white p-8">
+            <div className="mb-8">
+                <h1 className="text-3xl font-bold">Witaj na Dashboardzie!</h1>
+            </div>
+
+            {}
+             <div className="rounded-lg overflow-hidden border border-gray-600">
+                <MetabaseReport dashboardId={MY_DASHBOARD_ID} />
+            </div>
+        </main>
     );
 }
